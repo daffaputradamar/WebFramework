ca265f8f7abf25667e236ff8e48a5171f2f37d5a
Daffa Akbar <daffaputradamar@gmail.com>
2020-02-22 18:25:04 +0700

    Updates

--- precache-manifest.f603654f9a4cadf280b6370d944026ed.js
+++ precache-manifest.f603654f9a4cadf280b6370d944026ed.js
@@ -1,31 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c9bf2311bfbd8d1e8214dca4deae1d09",
+    "revision": "ae189293711dda1b1f6424111e820123",
     "url": "/WebFramework/index.html"
   },
   {
-    "revision": "1fd4e4abd70d69c185fa",
+    "revision": "19847eb34b1bc883a764",
     "url": "/WebFramework/static/css/2.47e06e2e.chunk.css"
   },
   {
-    "revision": "a22df61fe7563317408a",
+    "revision": "c2a0de3c6586d8167d69",
     "url": "/WebFramework/static/css/main.3728e709.chunk.css"
   },
   {
-    "revision": "1fd4e4abd70d69c185fa",
-    "url": "/WebFramework/static/js/2.7b4e3961.chunk.js"
+    "revision": "19847eb34b1bc883a764",
+    "url": "/WebFramework/static/js/2.e0eb1b11.chunk.js"
   },
   {
     "revision": "bfc08a62983de46a4daec9f8c3c657d8",
-    "url": "/WebFramework/static/js/2.7b4e3961.chunk.js.LICENSE.txt"
+    "url": "/WebFramework/static/js/2.e0eb1b11.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a22df61fe7563317408a",
-    "url": "/WebFramework/static/js/main.a191854c.chunk.js"
+    "revision": "c2a0de3c6586d8167d69",
+    "url": "/WebFramework/static/js/main.bdd7d143.chunk.js"
   },
   {
-    "revision": "d179c069cdcd768d7bd2",
-    "url": "/WebFramework/static/js/runtime-main.b90af76f.js"
+    "revision": "0b64304c1f667875b2e6",
+    "url": "/WebFramework/static/js/runtime-main.0882f80a.js"
   },
   {
     "revision": "668343985b9b112dd408f7a5e040bf48",
