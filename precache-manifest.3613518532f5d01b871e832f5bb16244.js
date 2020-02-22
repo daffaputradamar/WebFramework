2827c3fd5cc49bef177923d09c01b8b61d58c27e
Daffa Akbar <daffaputradamar@gmail.com>
2020-02-22 18:15:55 +0700

    Updates

---
+++ precache-manifest.3613518532f5d01b871e832f5bb16244.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd01e8ca86d2bb2f03a4e233651c59f",
+    "url": "/halo-react/index.html"
+  },
+  {
+    "revision": "13655c8faa4d1909c3c4",
+    "url": "/halo-react/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d905465757e8ae7337f9",
+    "url": "/halo-react/static/css/main.3728e709.chunk.css"
+  },
+  {
+    "revision": "13655c8faa4d1909c3c4",
+    "url": "/halo-react/static/js/2.dff56244.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/halo-react/static/js/2.dff56244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d905465757e8ae7337f9",
+    "url": "/halo-react/static/js/main.1ca0de69.chunk.js"
+  },
+  {
+    "revision": "e51b6871324d4e8b26dd",
+    "url": "/halo-react/static/js/runtime-main.5963504d.js"
+  },
+  {
+    "revision": "668343985b9b112dd408f7a5e040bf48",
+    "url": "/halo-react/static/media/foto.66834398.jpg"
+  },
+  {
+    "revision": "aa0ac4a9f201df5b4393c09b618389db",
+    "url": "/halo-react/static/media/laundrypintar.aa0ac4a9.png"
+  },
+  {
+    "revision": "ec43d5ffb29a8c2b84b41031363b8cf8",
+    "url": "/halo-react/static/media/mentorkoding.ec43d5ff.png"
+  },
+  {
+    "revision": "838c69a86af49458ffc8799c193d9da9",
+    "url": "/halo-react/static/media/rewangkerjo.838c69a8.png"
+  }
+]);
