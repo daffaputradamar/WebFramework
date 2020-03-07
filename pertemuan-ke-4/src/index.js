a982540edaa67a4a6ee9fac4ad4e897ef122518f
Daffa Akbar <daffaputradamar@gmail.com>
2020-03-07 16:13:31 +0700

    recover pertemuan-ke-4

--- pertemuan-ke-4/src/index.js
+++ pertemuan-ke-4/src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import Blog from "./pages/Blog";
 
 // function HelloWorld() {
 //   return <p>Ini adalah function component</p>;
@@ -21,7 +19,7 @@ import Blog from "./pages/Blog";
 // }
 
 // ReactDOM.render(<App />, document.getElementById("root"));
-ReactDOM.render(<Blog />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
